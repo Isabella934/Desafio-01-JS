2125e171f79e8e9c63eb11d751daf3e7cf63f082
Isabella934 <isabella.lessa27@gmail.com>
2022-06-09 20:36:23 -0300

    iniciando js

---
+++ script.js
@@ -0,0 +1,17 @@
+let nome = "Bella";
+console.log(nome);
+
+let idade = 19;
+console.log(idade);
+
+let number = 2022;
+console.log(number-idade);
+
+let soma = "2022";
+console.log(number+idade);
+
+let text = "Estou mexendo no console.log";
+console.log(text);
+
+let quartaFeira = true;
+console.log(typeof quartaFeira);
